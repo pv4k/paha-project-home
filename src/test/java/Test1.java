20165b2c01aad05dc12393f64f3a62f421677f34
voleniukp@gmail.com <voleniukp@gmail.com>
2023-05-24 11:41:10 -0400

    firstCommit

---
+++ src/test/java/Test1.java
@@ -0,0 +1,12 @@
+public interface Test1 {
+    public static void method1(){
+        System.out.println("Static");
+    }
+    public static void method2(){
+        System.out.println("Interface");
+    }
+
+public abstract void method3();
+
+   // public void method4();
+}
